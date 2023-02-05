86183eb40e103aae591f993a7e457b25a9d75dfe
PChunark <puttipongchunark@gmail.com>
2023-02-05 22:01:08 +0700

    Adjust labeling in axis, coloring fonts and theme

--- code/temperature_bar_plot.R
+++ code/temperature_bar_plot.R
@@ -15,13 +15,23 @@ t_data <- read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>%
           drop_na()
 
 # We need first and last year. So it needs to do programmatically
-t_data %>%
+annotation <- 
+  t_data %>%
   arrange(year) %>% #Arrange the data in order before selecting the data below
   slice(1, n()) %>% #Slice gives rows. We need to number. The code gives us first and last row of the dataframe
-  mutate(t_diff = 0) #Modify t_diff value to zero. We dont want year at the t_diff position value. We need it at zero position at the axis
-
+  mutate(t_diff = 0, #Modify t_diff value to zero. We dont want year at the t_diff position value. We need it at zero position at the axis
+           # Create new column to bump the 1880 and 2022 to the left and right
+         x = year + c(-5,5) #Add year coloumn a vector
+         )
 
 t_data %>% 
   ggplot(aes(x = year, y = t_diff))+ # Provide x and y parameter
   geom_col() + #geom_bar generates a bar plot with summary build-in function, we dont need!!
-  theme_void()
+  geom_text(data = annotation, aes(x = x, label = year), color = "white")+ #Add annotation, white font color
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = "black")
+  )
+
+
+ggsave("figures/temperature_bar_plot.png", width = 7, height = 4)
