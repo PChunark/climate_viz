a3337ac8b931f81937ffea87dfe5d22472da6eb7
PChunark <puttipongchunark@gmail.com>
2023-02-06 15:56:56 +0700

    Ignore NA values

--- code/temperature_lines.R
+++ code/temperature_lines.R
@@ -6,7 +6,8 @@ t_diff <-
   select(year = Year, month.abb) %>% #month.abb is constant R build-in. which is the same as our datatype
   # Tidy dataframe by converting month in 1 column accept for year
   #Pivot dataframe for all columns except for year column, Give a name of month column
-  pivot_longer(-year, names_to = "month", values_to = "t_diff")   
+  pivot_longer(-year, names_to = "month", values_to = "t_diff") %>%
+  drop_na() #Ignore NA value
 
 t_diff %>%
   ggplot(aes(x = month, y = t_diff, group = year, color = year)) +
