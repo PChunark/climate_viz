1b7b24d1af6a6631807bf2a265274508c52ee52c
PChunark <puttipongchunark@gmail.com>
2023-02-06 15:41:47 +0700

    Store selected data into a variable

--- code/temperature_lines.R
+++ code/temperature_lines.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 
-read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% #load CSV file, skip 1st rows
+
+t_diff <- 
+  read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% #load CSV file, skip 1st rows
   select(year = Year, month.abb) %>% #month.abb is constant R build-in. which is the same as our datatype
   # Tidy dataframe by converting month in 1 column accept for year
   #Pivot dataframe for all columns except for year column, Give a name of month column
