9d4ec969858057486b78925fad6adb6446f01765
PChunark <puttipongchunark@gmail.com>
2023-02-07 09:35:58 +0700

    Add comment

--- code/temperature_lines.R
+++ code/temperature_lines.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # 1. First plot
 t_diff <- 
-  read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% #load CSV file, skip 1st rows
+  read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% #load CSV file, skip 1st rows, na value in the data is "***"
   select(year = Year, month.abb) %>% #month.abb is constant R build-in. which is the same as our datatype
   # Tidy dataframe by converting month in 1 column accept for year
   #Pivot dataframe for all columns except for year column, Give a name of month column
