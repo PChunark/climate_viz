72203d00610a92a4c77fe00e5880dd862d129dec
PChunark <puttipongchunark@gmail.com>
2023-02-28 10:54:39 +0700

    Remove line size from aes in ggplot.

--- code/climate_spiral.R
+++ code/climate_spiral.R
@@ -57,8 +57,7 @@ t_data <-
   
   t_data %>% ggplot(aes(x = month_number, 
              y = t_diff, 
-             group = year, color = year, 
-             size = this_year)) + #Add size data
+             group = year, color = year)) + 
   geom_hline(yintercept = 0, color = "white") + # Add white line at 0 y intercept
   geom_line()+
   scale_x_continuous(breaks = 1:12, #Set x axis and its coordinator
