a4cdf98612c69fe69ab581cf6ba793a708cf90f1
PChunark <puttipongchunark@gmail.com>
2023-02-28 10:53:21 +0700

    Remove Geom_text.

--- code/climate_spiral.R
+++ code/climate_spiral.R
@@ -61,14 +61,6 @@ t_data <-
              size = this_year)) + #Add size data
   geom_hline(yintercept = 0, color = "white") + # Add white line at 0 y intercept
   geom_line()+
-  geom_text(data = annotation, # Add text to a plot
-            aes(x = month_number, 
-                y = t_diff, 
-                label = year, 
-                color = year), 
-            inherit.aes = FALSE,
-            hjust = 0, size = 5,
-            nudge_x = 0.1, fontface = "bold") + 
   scale_x_continuous(breaks = 1:12, #Set x axis and its coordinator
                      labels = month.abb,
                      sec.axis = dup_axis(name = NULL, labels = NULL)) +
