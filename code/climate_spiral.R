2e601bb0a54bf8ea1288bf8fcd520b022af642ce
PChunark <puttipongchunark@gmail.com>
2023-02-28 11:03:13 +0700

    Remove legend in scale color viridis.

--- code/climate_spiral.R
+++ code/climate_spiral.R
@@ -65,8 +65,7 @@ t_data <-
   scale_y_continuous(breaks = seq(-2,2,0.2), 
                      sec.axis = dup_axis(name = NULL, labels = NULL)) + #Rescale y axis
    scale_color_viridis_c(breaks = seq(1880,2020,20), #Change the color to continuous scale. Re-scale the legend
-                        guide = guide_colorbar(frame.colour = "white",
-                                               frame.linewidth = 1)) + #Add white border around the legend 
+                        guide = "none") + #Remove legend 
   # coord_cartesian(xlim = c(1,12)) +
   coord_polar()+  
   labs(x = NULL, # Add label
