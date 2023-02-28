ca3c88e3b565384e97fe65b86bf4e0199f57a216
PChunark <puttipongchunark@gmail.com>
2023-02-28 11:06:02 +0700

    Remove y-axis text.

--- code/climate_spiral.R
+++ code/climate_spiral.R
@@ -76,6 +76,7 @@ t_data <-
     plot.background = element_rect(fill = "#444444"),
     panel.grid = element_blank(), # Remove grid line color
     axis.text = element_text(color = "white", size = 13), # Add color text
+    axis.text.y = element_blank(),
     # axis.ticks = element_line(color = "white"), # Change tick mark color
     # axis.ticks.length = unit(-5, "pt"), #Put tick mark into the plot by 5 points
     axis.title = element_text(color = "white", size = 13),
