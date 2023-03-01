509c5bc2fefe7024af6666b6db2daf9e5ff214ac
PChunark <puttipongchunark@gmail.com>
2023-03-01 11:15:47 +0700

    Add comments

--- code/climate_spiral_animation.R
+++ code/climate_spiral_animation.R
@@ -78,7 +78,7 @@ month_label <-
              y = t_diff, 
              group = year, 
              color = year)) +
-    geom_rect(aes(xmin = 1, xmax = 13, ymin = -2, ymax = 2))+
+    geom_rect(aes(xmin = 1, xmax = 13, ymin = -2, ymax = 2))+ # create black circle background
   # geom_col(data = month_label, #Add black circle background
   #          aes(x = x + 0.5, y = 2.4),
   #          fill = "black",
