96f1682f0f44242e334710b75fe7643c7ebf1fde
PChunark <puttipongchunark@gmail.com>
2023-03-07 15:04:13 +0700

    Initial plot by using geom_path

--- code/spiral_in_cartesian_coordinate.R
+++ code/spiral_in_cartesian_coordinate.R
@@ -3,6 +3,9 @@ library(tidyverse)
 #Make dataframe to get radian in circle
 
 tibble(theta = 2 * pi * seq(0, 1, 0.05),
-       radius = 1) %>% 
-  ggplot(aes(x = theta, y = radius))+
-  geom_line()
+       radius = 1) %>%
+  mutate(x = radius * sin(theta),
+         y = radius * cos(theta)) %>% 
+  ggplot(aes(x = x, y = y))+
+#  geom_line() #geom_line connects the dot between plots in the order of the x-axis
+  geom_path()
