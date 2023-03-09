0de1a3e6be51209f1446752304cb7281f99c111b
PChunark <puttipongchunark@gmail.com>
2023-03-09 15:32:07 +0700

    Creating color gradient

--- code/climate_tornado.R
+++ code/climate_tornado.R
@@ -9,6 +9,11 @@ t_data <- read.csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>%
 t_data %>% 
   filter(month == "Apr" | month == "Oct") %>% 
   pivot_wider(names_from = "month", values_from = "t_diff") %>%  # to draw a geom_segment, we need to draw 4 lines.
-  ggplot(aes(x = -4 - Oct, xend = Apr, y = year, yend = year)) + 
-  geom_segment()
+  mutate(ave_t = (Oct + Apr) / 2) %>%  # Make an average temperature to provide a color to a line
+  ggplot(aes(x = -4 - Oct, xend = Apr, y = year, yend = year, color = ave_t)) + 
+  geom_segment() + 
+  scale_color_gradient2(low = "darkblue" , 
+                        mid = "white" , 
+                        high = "darkred",
+                        midpoint = 0)
   
