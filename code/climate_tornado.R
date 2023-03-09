3291f76376e6d62cb0b004066282fd20cfdd0aca
PChunark <puttipongchunark@gmail.com>
2023-03-09 15:36:14 +0700

    Creating vertical lines

--- code/climate_tornado.R
+++ code/climate_tornado.R
@@ -11,9 +11,12 @@ t_data %>%
   pivot_wider(names_from = "month", values_from = "t_diff") %>%  # to draw a geom_segment, we need to draw 4 lines.
   mutate(ave_t = (Oct + Apr) / 2) %>%  # Make an average temperature to provide a color to a line
   ggplot(aes(x = -4 - Oct, xend = Apr, y = year, yend = year, color = ave_t)) + 
+  geom_vline(xintercept = c(-5, -4, 0, 1), color = "gold") + #Creating vertical lines
   geom_segment() + 
   scale_color_gradient2(low = "darkblue" , 
                         mid = "white" , 
                         high = "darkred",
-                        midpoint = 0)
+                        midpoint = 0,
+                        guide = "none") 
+  
   
