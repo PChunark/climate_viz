85ce9db75159a9804c5cd8ac0cd795b7483383b7
PChunark <puttipongchunark@gmail.com>
2023-03-10 08:17:14 +0700

    Adjust the line thickness and round the line

--- code/climate_tornado.R
+++ code/climate_tornado.R
@@ -12,7 +12,7 @@ t_data %>%
   mutate(ave_t = (Oct + Apr) / 2) %>%  # Make an average temperature to provide a color to a line
   ggplot(aes(x = -4 - Oct, xend = Apr, y = year, yend = year, color = ave_t)) + 
   geom_vline(xintercept = c(-5, -4, 0, 1), color = "gold") + #Creating vertical lines
-  geom_segment() + 
+  geom_segment(size = 0.9, lineend = "round") + # Increase the thickness of the line. Round the end of the line
   scale_color_gradient2(low = "darkblue" , 
                         mid = "white" , 
                         high = "darkred",
