3fe9a0d2f1afd991eb32e60f0524d3aa3ec0f193
PChunark <puttipongchunark@gmail.com>
2023-03-10 13:25:03 +0700

    Draw a first plot

--- code/monthly_anomaly.R
+++ code/monthly_anomaly.R
@@ -5,3 +5,17 @@ month_anom <-
   select(month = Month, seas_anom) %>% 
   mutate(month = as.numeric(month),
          month = month.abb[month])
+
+t_data <- read_csv(file = "data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% 
+  select(year = Year, all_of(month.abb)) %>% 
+  pivot_longer(-year, names_to = "month", values_to = "t_diff") %>% 
+  drop_na() %>%  
+  inner_join(., month_anom, by = "month") %>%  #Join 2 dataframes by months
+  mutate(month = factor(month, levels = month.abb),
+         month_anom = t_diff + seas_anom) #Calculate month anom to generate a line plot
+
+t_data %>% 
+  ggplot(aes(x = month,
+             y = month_anom,
+             group = year)) + 
+  geom_line()
