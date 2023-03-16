1c7c1bf0d28dc1cd897b2d588f20be11b713d240
PChunark <puttipongchunark@gmail.com>
2023-03-16 12:32:28 +0700

    Reverse the bands to match the style

--- code/zonal_climate_anomalies.R
+++ code/zonal_climate_anomalies.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
 #The latitude zones are 90N-64N, 64N-44N, 44N-24N, 24N-EQU, EQU-24S, 24S-44S, 44S-64S, 64S-90S.
-bands <- c("64N-90N", "44N-64N", "24N-44N", "EQU-24N", 
-          "24S-EQU", "44S-24S", "64S-44S", "90S-64S")
+bands <- rev(c("64N-90N", "44N-64N", "24N-44N", "EQU-24N", 
+          "24S-EQU", "44S-24S", "64S-44S", "90S-64S")) #Rev argument is to reverse the vector
 
 # the original plot is from NASA: https://svs.gsfc.nasa.gov/4978
 url <- "https://data.giss.nasa.gov/gistemp/tabledata_v4/ZonAnn.Ts+dSST.csv"
