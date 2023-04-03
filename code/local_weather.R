3fb87dc71a81a30aba695df5fcb8ecaac8dee163
PChunark <puttipongchunark@gmail.com>
2023-04-03 15:20:27 +0700

    Find the nearest weather station

--- code/local_weather.R
+++ code/local_weather.R
@@ -10,9 +10,12 @@ inventory_url <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.
 
 inventory <-read_table(inventory_url,
                        col_names = c("station", "lat", "lon", "variable", "start", "end"))
-inventory
+# inventory
 
-#Find the location that we are living in. The location given in google map is in degree unit. We need to convert it to radian.
+#Find the location that we are living in. 
+# The location given in google map is in degree unit. We need to convert it to radian.
+# Location: EGAT T100 Building
+
 my_lat <- 13.811979809223814 * 2 * pi/360
 my_lon <- 100.5052562120295 * 2 * pi/360
 
@@ -23,7 +26,15 @@ my_lon <- 100.5052562120295 * 2 * pi/360
 # The obtained distance, d, is in miles. If you want your value to be in units of kilometers, multiple d by 1.609344.
 # d in kilometers = 1.609344 * d in miles
 
-inventory %>%
-  mutate(lat_radian = lat * 2 * pi/360,
-         lon_radian = lon * 2 * pi/360,
-         d = 3963 * acos((sin(lat_radian) * sin(my_lat)) + cos(lat_radian) * cos(my_lat) * cos(my_lon - lon_radian)))
+
+my_station <- inventory %>%
+              mutate(lat_radian = lat * 2 * pi/360,
+                     lon_radian = lon * 2 * pi/360,
+                     d_km = 1.609344 * 3963 * acos((sin(lat_radian) * sin(my_lat)) + cos(lat_radian) * cos(my_lat) * cos(my_lon - lon_radian))
+              ) %>%
+              filter(start < 1960 & end > 2020) %>% # Capture station name
+              top_n(n = -1, d_km) %>% #find the lowest distance
+              distinct(station) %>%  #get the weather station
+              pull(station) #%>% # Get station name as a variable
+              #arrange(d_km) #arrange d_km from the lowest to the highest value (Ascending order)
+
