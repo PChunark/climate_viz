d0654e3d8efaff278c7138995e8c4c4fa7b0eeb3
PChunark <puttipongchunark@gmail.com>
2023-04-07 11:18:06 +0700

    Hightlight data in 2022

--- code/local_precipitation.R
+++ code/local_precipitation.R
@@ -1,16 +1,27 @@
 source("code/local_weather.R")
 
+this_year <- year(today()) #Create an automate year
+
 local_weather %>% 
   select(date, prcp) %>%
   drop_na(prcp) %>% #drop NA value in prcp
   mutate(year = year(date),
          month = month(date),
-         day = day(date)) %>%
+         day = day(date),
+         is_this_year = year == this_year) %>% #Check whether "year" is equal to "this_year". It return TRUE & FALSE value. It is a conditional command.
   filter(!(month == 2 & day == 29)) %>% #Remove February 29th. Remove leap day
   group_by(year) %>% #Calculate cumulative prcp by year
   arrange(date) %>%  
   mutate(cum_prcp = cumsum(prcp)) %>%  #Calculate cumulative prcp by year
   ungroup() %>% 
   mutate(new_date = ymd(glue("2022-{month}-{day}"))) %>%  #Create Pseudo date???
-  ggplot(aes(x = new_date, y = cum_prcp, group = year))+
-  geom_step()
+  ggplot(aes(x = new_date, y = cum_prcp, group = year, 
+             color = is_this_year,
+             size = is_this_year))+
+  geom_step(show.legend = FALSE) + #Remove legend
+  scale_color_manual(breaks = c(F,T), #Give the color of lines
+                     values = c("lightgrey", "dodgerblue")) + 
+  scale_size_manual(breaks = c(F,T),
+                    values = c(0.3,0.5)) #Adjust the size of lines
+ 
+ggsave("figures/cumulative_prcp.png", width = 6, height = 5)
