d51beabd964815ca660db438bf16bb724d9c1caa
PChunark <puttipongchunark@gmail.com>
2023-04-08 23:58:00 +0700

    Assessing linear model with multiple variables

--- code/snow_ratio_model.R
+++ code/snow_ratio_model.R
@@ -27,13 +27,12 @@ cor.test(prcp_snow_annual$prcp, prcp_snow_annual$snow)
 #Create an daily precipitation and snow
 prcp_snow_daily <- local_weather %>% 
   drop_na() %>% 
-  filter(snow > 0) %>% 
+  filter(snow > 0 & tmax <=0) %>% 
   mutate(year = year(date)) 
   # filter(year != 2023) 
   
-  
+### 1. a linear model by default in geom_smooth ####  
 prcp_snow_daily%>% 
-  # filter(!snow >800 & !prcp >60) %>% 
   ggplot(aes(x = prcp, y = snow, color = tmax)) +
   geom_point() +
   geom_smooth(                         #by default it is a polynomial function. 
@@ -44,3 +43,23 @@ prcp_snow_daily%>%
   geom_abline(intercept = 0, slope = 10, size = 1) #This is the line that we can define an intercept and slope
 
 cor.test(prcp_snow_daily$prcp, prcp_snow_daily$snow)
+
+### 2. Build a linear model
+
+snow_model <- lm(snow~tmax*prcp+0, data = prcp_snow_daily)
+summary(snow_model)
+
+#Predict the value by using snow_model in the prcp_snow_daily
+predict(snow_model, prcp_snow_daily)
+
+prcp_snow_daily%>% 
+  mutate(predicted_snow = predict(snow_model, prcp_snow_daily)) %>% 
+  ggplot(aes(x = prcp, y = snow, color = tmax)) +
+  geom_point() +
+  geom_smooth(                         #by default it is a polynomial function. 
+    formula = "y~x+0", #We do a linear regression. Assumed that 0 snow is 0 precipitation. We add an intercept = 0.
+    method = "lm", #this is linear regression method.
+    se = FALSE
+  ) +
+  geom_abline(intercept = 0, slope = 10, size = 1) + #This is the line that we can define an intercept and slope
+  geom_smooth(aes(y = predicted_snow), se = FALSE, color ="red")
