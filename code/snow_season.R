79a287e37828286db044919bc51543156a379d06
PChunark <puttipongchunark@gmail.com>
2023-04-09 10:08:37 +0700

    Added comments

--- code/snow_season.R
+++ code/snow_season.R
@@ -5,7 +5,7 @@ local_weather %>%
   drop_na(snow) %>% 
   mutate(calendar_year = year(date),
          month = month(date),
-         snow_year = if_else(date < ymd(glue("{calendar_year}-07-01")),
+         snow_year = if_else(date < ymd(glue("{calendar_year}-07-01")), #Snow year in USA or in Canada is in July - June
                                         calendar_year - 1,
                                         calendar_year)
          ) %>% 
