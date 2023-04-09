e200e5e909eff6597105a3131eaad57fa50a8953
PChunark <puttipongchunark@gmail.com>
2023-04-09 10:07:38 +0700

    Calculate snow year

--- code/snow_season.R
+++ code/snow_season.R
@@ -3,8 +3,11 @@ source("code/local_weather.R")
 local_weather %>% 
   select(date, snow) %>% 
   drop_na(snow) %>% 
-  mutate(cal_year = year(date),
-         month = month(date)
+  mutate(calendar_year = year(date),
+         month = month(date),
+         snow_year = if_else(date < ymd(glue("{calendar_year}-07-01")),
+                                        calendar_year - 1,
+                                        calendar_year)
          ) %>% 
-  filter(!cal_year == 1939)  
+  filter(!calendar_year == 1939)  
   
