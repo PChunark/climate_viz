b854bfbeda88b6873ec076e711d6a80cfc28ca3a
PChunark <puttipongchunark@gmail.com>
2023-11-23 15:41:43 +0700

    define lag on date column

--- code/drought_window.R
+++ code/drought_window.R
@@ -6,4 +6,6 @@ local_weather %>%
   select(date, prcp) %>% 
   mutate(prcp = replace_na(prcp, 0)) %>% # other option to replace NA
   # replace_na(list(prcp = 0)) %>%  # replace NA argument should be a list
-  filter(prcp > threshold)
+  filter(prcp > threshold) %>% 
+  mutate(prev_date = lag(date, n = 1)) %>% 
+  drop_na()
