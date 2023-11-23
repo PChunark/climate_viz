070771c9677dd21334be9897ebf41e654e391a0a
PChunark <puttipongchunark@gmail.com>
2023-11-23 15:38:09 +0700

    Filtering out no or low precipitation

--- code/drought_window.R
+++ code/drought_window.R
@@ -1,6 +1,9 @@
 source("code/local_weather.R")
 
+threshold <- 0 
+
 local_weather %>% 
   select(date, prcp) %>% 
   mutate(prcp = replace_na(prcp, 0)) %>% # other option to replace NA
   # replace_na(list(prcp = 0)) %>%  # replace NA argument should be a list
+  filter(prcp > threshold)
