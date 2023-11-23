65d5dd6f79de401c8363c6dd0176c7ed0d20f02b
PChunark <puttipongchunark@gmail.com>
2023-11-23 15:46:56 +0700

    Calculating the lenght of the drought

--- code/drought_window.R
+++ code/drought_window.R
@@ -2,10 +2,14 @@ source("code/local_weather.R")
 
 threshold <- 0 
 
-local_weather %>% 
+drought_by_year<-
+  local_weather %>% 
   select(date, prcp) %>% 
   mutate(prcp = replace_na(prcp, 0)) %>% # other option to replace NA
   # replace_na(list(prcp = 0)) %>%  # replace NA argument should be a list
   filter(prcp > threshold) %>% 
   mutate(prev_date = lag(date, n = 1)) %>% 
-  drop_na()
+  drop_na() %>% 
+  mutate(drought_length = as.numeric(date-prev_date),#find a drought length
+         year = year(date)) %>% 
+  select(year, length = drought_length)
